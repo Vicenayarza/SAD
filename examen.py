77e364efb94f9ead5ef7eb4a84e14ce6517137b8
Vicenayarza <73492919+Vicenayarza@users.noreply.github.com>
2023-03-24 08:25:49 +0100

    Add files via upload

---
+++ examen.py
@@ -0,0 +1,15 @@
+#--PARA VER LOS ENTORNOS--
+#anaconda-navigator
+#--ELEGIR ENTORNO--
+#conda activate sad 
+#--LIBRERIAS--
+#pip3 install pickle5
+#pip3 install imblearn
+#pip3 install pandas
+#pip3 install scikit-learn
+#pip3 install datetime--solo si da error 
+#--EJECUTAR RECETAS--
+#python plantilla.py -k 1  -d 1 -m uniform -o prueba.csv
+
+#barrido /.examen.sh
+#python PlantillaArbolesSinPreprocesar.py -m 3 -s 2 -l 1 -o fsinPro.csv
